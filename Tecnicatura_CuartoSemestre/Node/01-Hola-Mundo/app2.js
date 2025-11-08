d11986ca71462e8d30294c72a8ce081ed8f1258d
Lucas Sosa <gastonluc01@gmail.com>
2025-11-08 02:06:18 -0300

    Clases NODE

---
+++ Tecnicatura_CuartoSemestre/Node/01-Hola-Mundo/app2.js
@@ -0,0 +1,5 @@
+
+const saludar = (nombre) => { //Funcion de flecha
+    return `Saludos de ${nombre}`; //Usamos backticks ``
+};//esto es igual a: 'Saludos de ' + nombre
+console.log(saludar('Lucas'));
---
+++ Tecnicatura_CuartoSemestre/Node/01-Hola-Mundo/entorno.js
@@ -0,0 +1,7 @@
+
+let nombre =process.env.NOMBRE || 'Sin Nombre';
+let web = process.env.WEB || 'Sin Web';
+console.log('hola '+nombre);
+console.log('mi web es '+web);
+
+console.log('Ultima parte');
