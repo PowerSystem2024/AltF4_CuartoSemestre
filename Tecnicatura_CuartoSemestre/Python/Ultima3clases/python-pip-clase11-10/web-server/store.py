99706dd92b93cc08d7232bc83649996fa312ccf0
juanivera12 <eljuanivera@gmail.com>
2025-11-10 15:49:00 -0300

    Agrego las ultimas 3 clases de python

---
+++ Tecnicatura_CuartoSemestre/Python/Ultima3clases/python-pip-clase11-10/web-server/store.py
@@ -0,0 +1,8 @@
+import requests
+def get_razas():
+    r = requests.get("https://dog.ceo/api/breeds/list/all")
+    print(r.status_code)
+    print(type(r.text)) 
+    razas = r.json()
+    for raza in razas.values():
+        print(f"Raza de los perritos: {raza}")
